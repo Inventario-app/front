09e92691074aa16c15cc222f764ff5c4b32f498d
Jordy <j.joao.gonza@gmail.com>
2025-02-15 21:51:57 -0600

    Add Axios to dependencies

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import { onMounted } from "vue";
+</script>
 <template>
   <div>
     <img class="size-40" src="../assets/Inventario-NoBG-Logo.png" alt="" />
