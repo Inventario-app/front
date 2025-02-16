1e6d4186ce244755a6ee9bfb012f7ba36968c4fe
Jordy <j.joao.gonza@gmail.com>
2025-02-15 21:48:16 -0600

    Start Login Page

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup>
+import Logo from "./components/Logo.vue";
+</script>
+<template>
+  <Logo class="flex justify-around" />
+</template>
---
+++ src/components/Logo.vue
@@ -0,0 +1,6 @@
+<script setup></script>
+<template>
+  <div>
+    <img class="size-40" src="../assets/Inventario-NoBG-Logo.png" alt="" />
+  </div>
+</template>
